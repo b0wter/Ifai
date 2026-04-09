c6ab0f7adf96a0d2f6e9f613956d3b3653fec474
b0wter <b0wter@gmx.de>
2026-04-09 22:51:19 +0200

    Fixed broken ID creation (were creating the wrong type)

--- lib/Ids.fs
+++ lib/Ids.fs
@@ -1,28 +1,45 @@
 namespace Ifai.Lib
 
+open System
+
 type RoomId = RoomId of string
 module RoomId =
-    let create g = g |> RoomId 
+    let create id =
+        if id |> String.IsNullOrWhiteSpace
+        then failwith "Cannot create room id from empty string"
+        else id |> RoomId 
     let value (RoomId r) = r
 
 type CharacterId = CharacterId of string
 module CharacterId =
-    let create id = CharacterId id
+    let create id =
+        if id |> String.IsNullOrWhiteSpace
+        then failwith "Cannot create character id from empty string"
+        else id |> CharacterId
     let value (CharacterId id) = id
 
 type SpellId = SpellId of string
 module SpellId =
-    let create id = SpellId id
+    let create id =
+        if id |> String.IsNullOrWhiteSpace
+        then failwith "Cannot create spell id from empty string"
+        else id |> SpellId
     let value (SpellId id) = id
 
 type ThingId = ThingId of string
 module ThingId =
-    let create id = id |> ThingId
+    let create id =
+        if id |> String.IsNullOrWhiteSpace
+        then failwith "Cannot create thing id from empty string"
+        else id |> ThingId
     let value (ThingId i) = i
 
 type SpellInstanceId = SpellInstanceId of string
 module SpellInstanceId =
-    let create id = id |> SpellInstanceId
+    let create id =
+        if id |> String.IsNullOrWhiteSpace
+        then failwith "Cannot create spell-instance id from empty string"
+        else id |> SpellInstanceId
     let value (SpellInstanceId i) = i
 
 type MetaId =
